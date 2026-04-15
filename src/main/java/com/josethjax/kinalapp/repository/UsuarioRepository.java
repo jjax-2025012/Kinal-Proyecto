033faf8a0e419e57f50f1aad358bae6dd75f3f94
jjax-2025012 <jjax-2025012@kinal.edu.gt>
2026-04-14 21:50:14 -0600

    fix: asegurar que UsuarioRepository use Integer como tipo de ID

--- src/main/java/com/josethjax/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/josethjax/kinalapp/repository/UsuarioRepository.java
@@ -3,6 +3,5 @@ package com.josethjax.kinalapp.repository;
 import com.josethjax.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository <Usuario, Integer>{
-
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {  // Cambiado de Integer a Integer (ya está bien)
 }

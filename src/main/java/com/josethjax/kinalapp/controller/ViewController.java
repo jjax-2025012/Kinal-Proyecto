b52c17f81f97ffb80e669f7da752abe731ddc9f7
jjax-2025012 <jjax-2025012@kinal.edu.gt>
2026-05-12 09:03:49 -0600

    feat: Protección de rutas con Spring Security

--- src/main/java/com/josethjax/kinalapp/controller/ViewController.java
+++ src/main/java/com/josethjax/kinalapp/controller/ViewController.java
@@ -1,6 +1,8 @@
 package com.josethjax.kinalapp.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -15,4 +17,15 @@ public class ViewController {
     public String index() {
         return "index";
     }
+
+    @Controller
+    public class ErrorController {
+        @GetMapping("/403")
+        public String accessDenied(Model model, HttpServletRequest request) {
+            // Por si queremos pasar un mensaje dinámico
+            model.addAttribute("mensaje", "Solo los administradores pueden realizar esta acción");
+            return "error/403";
+        }
+    }
+
 }
